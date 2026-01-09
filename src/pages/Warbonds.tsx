24b4e7c2ef77f8abbfc08108d12c65caaa32e18a
keala <K13DOHERTY@student.bridgew.edu>
2026-01-08 21:42:02 -0500

    Premium Warbonds are finished next is to do legendary warbonds tomorrow and add styling to top of header page then deploy iniy verison!!!!

--- src/pages/Warbonds.tsx
+++ src/pages/Warbonds.tsx
@@ -4,6 +4,17 @@ import ChemicalAgents from './../assets/chemical agents.png';
 import Helldivers from './../assets/Helldivers_Mobilize_Warbond_Cover.jpg';
 import forceoflaw from './../assets/force of law.jpg';
 import justice from './../assets/borderline justice.jpg';
+import polar from './../assets/polar patriots.jpg';
+import urban from './../assets/urban legends.jpg';
+import cutting from './../assets/CUTTING_EDGE_PREMIUM_WARBOND.jpg';
+import steeled from './../assets/steeled-veterans-warbond.jpg';
+import viper from './../assets/viper commandos.jpg'
+import truth from './../assets/truth-enforcers.jpg';
+import demo from './../assets/democratic-detonation.jpg';
+import masters from './../assets/mastersofcermony.jpg';
+import dust from './../assets/dust devils.jpg';
+import control from './../assets/control group.jpg';
+import python from './../assets/python commandos.jpg';
 import './warbonds.css';
 
 const Warbonds = () => {
@@ -73,9 +84,7 @@ const Warbonds = () => {
                 <h1 className="forceHeader">
                     Force of Law
                 </h1>
-            </div>
-
-            <div className="imgcontainer" >
+            
                 <h1 className="justiceHeader">
                     Borderline Justice
                 </h1>
@@ -83,7 +92,104 @@ const Warbonds = () => {
 
             <div className='imgcontainer'>
                 <img src ={forceoflaw} className ='forceoflaw' />
+
+
                 <img src ={justice} className ='justice' />
+
+            </div>
+
+
+
+
+            <div className ='imgcontainer'>
+                <h1 className='urbanHeader'> 
+                    Urban Legends
+                </h1>
+
+                <h1 className='polarHeader'>
+                    Polar Patriots
+                </h1>
+            </div>
+
+            <div className='imgcontainer'>
+                <img src = {urban} className="urban" />
+                <img src = {polar} className='polar' />
+            </div>
+
+            <div className="imgcontainer">
+                <h1 className="steeledHeader">
+                    Steeled Veterans
+                </h1>
+
+                <h1 className="cuttingHeader">
+                    Cutting Edge
+                </h1>
+            </div>
+
+            <div className="imgcontainer">
+                <img src = {steeled} className = 'steeled' />
+
+                <img src = {cutting} className='cuttingEdge' />
+            </div>
+
+            <div className="imgcontainer">
+                <h1 className="viperHeader">
+                    Viper Commandos
+                </h1>
+
+                <h1 className="truthHeader">
+                    Truth Enforcers
+                </h1>
+            </div>
+
+            <div className="imgcontainer">
+                <img src = {viper} className="viper" />
+                <img src = {truth} className='truth' />
+            </div>
+
+
+            <div className="imgcontainer">
+                <h1 className="demoHeader">
+                    Democratic Detonation
+                </h1>
+
+                <h1 className="mastersHeader">
+                    Masters of Ceromony
+                </h1>
+            </div>
+
+            <div className="imgcontainer">
+                <img src = {demo} className="demo" />
+                <img src = {masters} className="masters" />
+            </div>
+
+            <div className="imgcontainer">
+                <h1 className ='dustHeader'>
+                    Dust Devils
+                </h1>
+
+                <h1 className="controlHeader">
+                    Control Group
+                </h1>
+            </div>
+
+            <div className="imgcontainer">
+                <img src = {dust} className="dust" />
+                <img src = {control} className="control" />
+            </div>
+
+            <div className="imgcontainer">
+                <h1 className="pythonHeader">
+                    Python Commandos
+                </h1>
+
+                <h1 className="comingSoon">
+                    Coming Soon Helldivers
+                </h1>
+            </div>
+
+            <div className="imgContainer">
+                <img src ={python} className="python" />
 
             </div>
 
